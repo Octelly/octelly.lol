fdde56e5870b5d7aa9cc915fdf72e783f13f77e2
Octelly <eli@stefek.cz>
2023-06-27 00:04:18 +0200

    forgor i still had the stupib old testing stuff on there

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Icon from '@iconify/svelte';
-    import accountIcon from '@iconify/icons-mdi/account';
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p><Icon icon={accountIcon} class="inline"/> Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
